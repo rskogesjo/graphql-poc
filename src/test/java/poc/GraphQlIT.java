b78d4f22ca6130693f0ac8a8025b9e43f4b9ba36
robin.skogesjo <robin.skogesjo@atg.se>
2020-09-03 20:37:43 +0200

    hamcrest

--- src/test/java/poc/GraphQlIT.java
+++ src/test/java/poc/GraphQlIT.java
@@ -14,8 +14,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import java.io.IOException;
 
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
-import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 
 @RunWith(SpringRunner.class)
