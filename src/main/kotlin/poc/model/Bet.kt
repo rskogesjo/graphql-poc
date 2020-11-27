56aef24d7e7ebeca27873d77afce21d413d6555c
robin.skogesjo <robin.skogesjo@atg.se>
2020-11-27 07:14:56 +0100

    example of subscription

---
+++ src/main/kotlin/poc/model/Bet.kt
@@ -0,0 +1,3 @@
+package poc.model
+
+data class Bet(val horse: String, val amount: Int, val timestamp: String)
---
+++ src/main/kotlin/poc/resolver/PersonSubscriptionResolver.kt
@@ -0,0 +1,15 @@
+package poc.resolver
+
+import com.coxautodev.graphql.tools.GraphQLSubscriptionResolver
+import org.reactivestreams.Publisher
+import org.springframework.stereotype.Component
+import poc.model.Bet
+import reactor.core.publisher.Flux.interval
+import java.lang.System.nanoTime
+import java.time.Duration.ofSeconds
+import kotlin.random.Random
+
+@Component
+class PersonSubscriptionResolver : GraphQLSubscriptionResolver {
+    fun bet(horse: String): Publisher<Bet> = interval(ofSeconds(3)).map { Bet(horse, Random.nextInt(), nanoTime().toString()) }
+}
