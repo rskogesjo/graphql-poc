e69ffb66972035e65b8b0c628786dccc7f99438b
robin.skogesjo <robin.skogesjo@atg.se>
2020-11-29 17:10:00 +0100

    Test for subscription

--- src/test/kotlin/poc/GraphQlIT.kt
+++ src/test/kotlin/poc/GraphQlIT.kt
@@ -1,6 +1,12 @@
 package poc
 
+import com.fasterxml.jackson.databind.JsonNode
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
+import com.fasterxml.jackson.module.kotlin.registerKotlinModule
 import com.graphql.spring.boot.test.GraphQLTestTemplate
+import graphql.servlet.internal.GraphQLRequest
+import org.awaitility.Awaitility.await
 import org.hamcrest.CoreMatchers.equalTo
 import org.hamcrest.MatcherAssert.assertThat
 import org.junit.Assert.assertTrue
@@ -11,13 +17,24 @@ import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.annotation.DirtiesContext
 import org.springframework.test.annotation.DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD
 import org.springframework.test.context.junit4.SpringRunner
+import org.springframework.web.socket.TextMessage
+import org.springframework.web.socket.WebSocketMessage
+import org.springframework.web.socket.WebSocketSession
+import org.springframework.web.socket.client.standard.StandardWebSocketClient
+import org.springframework.web.socket.handler.TextWebSocketHandler
+import org.springframework.web.socket.messaging.WebSocketStompClient
+import util.TestUtils
+import java.time.Duration
+
 
 @RunWith(SpringRunner::class)
-@SpringBootTest(classes = [Application::class], webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest(classes = [Application::class], webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @DirtiesContext(classMode = BEFORE_EACH_TEST_METHOD)
 class GraphQlIT {
     @Autowired
     private lateinit var graphQLTestTemplate: GraphQLTestTemplate
+
+    private val objectMapper = jacksonObjectMapper().registerKotlinModule()
 
     @Test
     fun `can create person`() {
@@ -62,7 +79,27 @@ class GraphQlIT {
 
     @Test
     fun `can subscribe`() {
-        val rawResponse = graphQLTestTemplate.postForResource("graphql/subscription.graphql")
-        assertTrue(rawResponse.isOk)
+        val client = StandardWebSocketClient()
+        val stompClient = WebSocketStompClient(client)
+        val response = SubscriptionResponse()
+
+        val webSocketSession = stompClient.webSocketClient.doHandshake(object : TextWebSocketHandler() {
+            override fun handleMessage(session: WebSocketSession, message: WebSocketMessage<*>) {
+                response.horse = objectMapper.readValue<JsonNode>(message.payload as String)
+                        .path("data")
+                        .path("bet")
+                        .path("horse")
+                        .asText()
+            }
+        }, "ws://localhost:8080/subscriptions").get()
+
+        val query = TestUtils.readTestData("graphql/subscription.graphql")
+        val request = GraphQLRequest(query, mapOf(), "")
+
+        webSocketSession.sendMessage(TextMessage(objectMapper.writeValueAsString(request)))
+
+        await().atMost(Duration.ofSeconds(5)).until { response.horse == "Lucky Boko" }
     }
+
+    private class SubscriptionResponse(var horse: String = "")
 }
---
+++ src/test/kotlin/util/TestUtils.kt
@@ -0,0 +1,10 @@
+package util
+
+object TestUtils {
+    fun readTestData(filename: String) = TestUtils.javaClass
+            .getResource("/$filename")
+            .openStream()
+            .buffered()
+            .reader()
+            .use { reader -> reader.readText() }
+}
