ab992cb86752741d98681f6aafd761b4f262c109
robin.skogesjo <robin.skogesjo@atg.se>
2020-11-29 20:06:38 +0100

    Trying out AssertJ removed warning from JPA

--- src/test/kotlin/poc/GraphQlIT.kt
+++ src/test/kotlin/poc/GraphQlIT.kt
@@ -6,9 +6,8 @@ import com.fasterxml.jackson.module.kotlin.readValue
 import com.fasterxml.jackson.module.kotlin.registerKotlinModule
 import com.graphql.spring.boot.test.GraphQLTestTemplate
 import graphql.servlet.internal.GraphQLRequest
+import org.assertj.core.api.Assertions.assertThat
 import org.awaitility.Awaitility.await
-import org.hamcrest.CoreMatchers.equalTo
-import org.hamcrest.MatcherAssert.assertThat
 import org.junit.Assert.assertTrue
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -26,7 +25,6 @@ import org.springframework.web.socket.messaging.WebSocketStompClient
 import util.TestUtils
 import java.time.Duration.ofSeconds
 
-
 @RunWith(SpringRunner::class)
 @SpringBootTest(classes = [Application::class], webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @DirtiesContext(classMode = BEFORE_EACH_TEST_METHOD)
@@ -43,10 +41,11 @@ class GraphQlIT {
         val response = rawResponse.readTree()
         val createdPersonNode = response.path("data").path("createPerson")
 
+
         assertTrue(rawResponse.isOk)
-        assertThat(createdPersonNode.path("id").asInt(), equalTo(1))
-        assertThat(createdPersonNode.path("name").asText(), equalTo("Robin"))
-        assertThat(createdPersonNode.path("age").asInt(), equalTo(34))
+        assertThat(createdPersonNode.path("id").asInt()).isEqualTo(1)
+        assertThat(createdPersonNode.path("name").asText()).isEqualTo("Robin")
+        assertThat(createdPersonNode.path("age").asInt()).isEqualTo(34)
     }
 
     @Test
@@ -59,7 +58,7 @@ class GraphQlIT {
         val response = rawResponse.readTree()
 
         assertTrue(rawResponse.isOk)
-        assertThat(response.path("data").path("getAll").size(), equalTo(expectedNumberOfPeople))
+        assertThat(response.path("data").path("getAll").size()).isEqualTo(expectedNumberOfPeople)
     }
 
     @Test
@@ -72,9 +71,9 @@ class GraphQlIT {
         val singlePersonNode = response.path("data").path("getOne")
 
         assertTrue(rawResponse.isOk)
-        assertThat(singlePersonNode.path("id").asInt(), equalTo(1))
-        assertThat(singlePersonNode.path("name").asText(), equalTo("Robin"))
-        assertThat(singlePersonNode.path("age").asInt(), equalTo(34))
+        assertThat(singlePersonNode.path("id").asInt()).isEqualTo(1)
+        assertThat(singlePersonNode.path("name").asText()).isEqualTo("Robin")
+        assertThat(singlePersonNode.path("age").asInt()).isEqualTo(34)
     }
 
     @Test
