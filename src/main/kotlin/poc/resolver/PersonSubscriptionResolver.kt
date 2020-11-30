f6ed9a8a3c62ddd250d537ab950a35d93b0ac673
robin.skogesjo <robin.skogesjo@atg.se>
2020-11-30 09:32:47 +0100

    static import

--- src/main/kotlin/poc/resolver/PersonSubscriptionResolver.kt
+++ src/main/kotlin/poc/resolver/PersonSubscriptionResolver.kt
@@ -5,6 +5,7 @@ import org.reactivestreams.Publisher
 import org.springframework.stereotype.Component
 import poc.model.Bet
 import reactor.core.publisher.Flux.interval
+import java.lang.System.currentTimeMillis
 import java.lang.System.nanoTime
 import java.time.Duration.ofSeconds
 import kotlin.random.Random
@@ -12,6 +13,6 @@ import kotlin.random.Random
 @Component
 class PersonSubscriptionResolver : GraphQLSubscriptionResolver {
     fun bet(horse: String): Publisher<Bet> = interval(ofSeconds(2)).map {
-        Bet(horse = horse, amount = Random(System.currentTimeMillis()).nextInt(1, 1000), timestamp = nanoTime().toString())
+        Bet(horse = horse, amount = Random(currentTimeMillis()).nextInt(1, 1000), timestamp = nanoTime().toString())
     }
 }
