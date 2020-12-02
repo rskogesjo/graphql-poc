e7af94e6845c63354ba53d23d77254e1b4a4c95a
robin.skogesjo <robin.skogesjo@atg.se>
2020-12-02 18:04:11 +0100

    Removed superfluous annotations for JSON property

--- src/main/kotlin/poc/model/Bet.kt
+++ src/main/kotlin/poc/model/Bet.kt
@@ -1,9 +1,3 @@
 package poc.model
 
-import com.fasterxml.jackson.annotation.JsonProperty
-
-data class Bet(
-        @get:JsonProperty("horse") val horse: String,
-        @get:JsonProperty("amount") val amount: Int,
-        @get:JsonProperty("timestamp") val timestamp: String
-)
+data class Bet(val horse: String, val amount: Int, val timestamp: String)
