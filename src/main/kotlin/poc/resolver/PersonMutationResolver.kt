63698bd9dbaf1abc8404caab0f75f993f4774e20
robin.skogesjo <robin.skogesjo@atg.se>
2021-02-07 22:18:24 +0100

    remove unused dependency

--- src/main/kotlin/poc/resolver/PersonMutationResolver.kt
+++ src/main/kotlin/poc/resolver/PersonMutationResolver.kt
@@ -1,16 +1,12 @@
 package poc.resolver
 
 import com.coxautodev.graphql.tools.GraphQLMutationResolver
-import org.dataloader.DataLoader
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
 import poc.model.Person
 import poc.repository.PersonRepository
 
 @Component
-class PersonMutationResolver(
-    @Autowired private val repository: PersonRepository,
-    @Autowired private val personDataLoader: DataLoader<Int, Person>
-) : GraphQLMutationResolver {
+class PersonMutationResolver(@Autowired private val repository: PersonRepository) : GraphQLMutationResolver {
     fun createPerson(name: String, age: Int) = repository.save(Person(name = name, age = age))
 }
